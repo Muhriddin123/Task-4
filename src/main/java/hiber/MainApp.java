cf4eb604477974e650f3254b0f741ab301a18d0f
MMamajonov <mamajonov_muhriddin@mail.ru>
2023-10-28 01:09:21 +0600

    Task

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,34 +1,45 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        User user1 = new User("Muhriddin", "Mamajonov", "mamajonov_muhriddin@mail.ru");
+        User user2 = new User("Oleg", "Tinkoff", "mamajonov_muhriddin@mail.ru");
+        Car car1 = new Car("Mersedes", 2023);
+        Car car2 = new Car("BMW", 2022);
+        user1.setCar(car1);
+        car1.setUser(user1);
+        user2.setCar(car2);
+        car2.setUser(user2);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
 
-      context.close();
-   }
+        userService.add(user1);
+        userService.add(user2);
+
+
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println(user + " " + user.getCar());
+        }
+
+       System.out.println(userService.getUserByCarModelAndSeries("BMW", 2022));
+        System.out.println(userService.getUserByCarModelAndSeries("Mersedes", 2023));
+
+
+    }
 }
